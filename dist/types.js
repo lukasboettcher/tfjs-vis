28597f5a5e02f1f58dc95b33cf19a7861fdefc22
Lukas Boettcher <1340215+lukasboettcher@users.noreply.github.com>
2022-02-26 02:21:21 +0100

    add dist folder as it is needed for the pkg install

---
+++ dist/types.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function isSurfaceInfo(drawable) {
+    if (drawable.name != null) {
+        return true;
+    }
+    return false;
+}
+exports.isSurfaceInfo = isSurfaceInfo;
+function isSurface(drawable) {
+    if (drawable.drawArea instanceof HTMLElement) {
+        return true;
+    }
+    return false;
+}
+exports.isSurface = isSurface;
